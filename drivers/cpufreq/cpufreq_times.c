1583d7b2399bac850308d2ab177529c55520ef80
Connor O'Brien <connoro@google.com>
2018-01-19 14:24:05 -0800

    ANDROID: cpufreq: times: add all_time_in_state file
    Add all_time_in_state sysfs node, which prints time in state data for each
    cpu and each available frequency. To facilitate this, create a global
    frequency array to support iterating over all available frequencies in
    a consistent order.
    
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Bug: 72339335
    Test: Read /sys/devices/system/cpu/cpufreq/all_time_in_state
    Change-Id: I54b6abf20bd1c0397f373115d184a9c6cf370aa6

--- drivers/cpufreq/cpufreq_times.c
+++ drivers/cpufreq/cpufreq_times.c
@@ -18,14 +18,18 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 
-static spinlock_t cpufreq_times_lock;
+static DEFINE_SPINLOCK(cpufreq_times_lock);
 
 struct cpufreq_times {
 	unsigned long long last_time;
 	unsigned int max_state;
 	unsigned int last_index;
+	unsigned int offset;
 	u64 *time_in_state;
 };
+
+static unsigned int next_offset;
+static unsigned int *cpufreq_states;
 
 static int cpufreq_times_update(struct cpufreq_times *times)
 {
@@ -59,6 +63,58 @@ static ssize_t show_time_in_state(struct cpufreq_policy *policy, char *buf)
 	return len;
 }
 
+static ssize_t show_all_time_in_state(struct kobject *kobj,
+				      struct kobj_attribute *attr, char *buf)
+{
+	ssize_t len = 0;
+	unsigned int i, cpu, freq;
+	bool found;
+	u64 time;
+	struct cpufreq_policy *policy;
+	struct cpufreq_times *times;
+
+	len += scnprintf(buf + len, PAGE_SIZE - len, "freq\t\t");
+	for_each_possible_cpu(cpu) {
+		len += scnprintf(buf + len, PAGE_SIZE - len, "cpu%u\t\t", cpu);
+		policy = cpufreq_cpu_get(cpu);
+		if (!policy)
+			continue;
+		if (policy->times)
+			cpufreq_times_update(policy->times);
+		cpufreq_cpu_put(policy);
+	}
+
+	spin_lock(&cpufreq_times_lock);
+	for (i = 0; i < next_offset; i++) {
+		freq = cpufreq_states[i];
+		len += scnprintf(buf + len, PAGE_SIZE - len, "\n%u\t\t", freq);
+		for_each_possible_cpu(cpu) {
+			found = false;
+			policy = cpufreq_cpu_get(cpu);
+			if (policy) {
+				times = policy->times;
+				if (times && times->offset <= i &&
+				    i < times->offset + times->max_state) {
+					time = times->time_in_state[i - times->offset];
+					found = true;
+				}
+				cpufreq_cpu_put(policy);
+			}
+			if (found) {
+				len += scnprintf(buf + len, PAGE_SIZE - len,
+						 "%lu\t\t", (unsigned long)
+						 cputime64_to_clock_t(time));
+			} else {
+				len += scnprintf(buf + len, PAGE_SIZE - len,
+						 "N/A\t\t");
+			}
+		}
+	}
+	spin_unlock(&cpufreq_times_lock);
+	len += scnprintf(buf + len, PAGE_SIZE - len, "\n");
+	return len;
+}
+
 cpufreq_freq_attr_ro(time_in_state);
 
 static struct attribute *default_attrs[] = {
@@ -70,12 +126,16 @@ static struct attribute_group times_attr_group = {
 	.name = "stats"
 };
 
+static struct kobj_attribute _attr_all_time_in_state = __ATTR(all_time_in_state,
+		0444, show_all_time_in_state, NULL);
+
 static int cpufreq_times_create_table(struct cpufreq_policy *policy)
 {
 	int ret;
 	unsigned int count = 0;
 	struct cpufreq_times *times;
 	struct cpufreq_frequency_table *pos, *table;
+	void *tmp;
 
 	if (policy->times)
 		return -EEXIST;
@@ -100,6 +160,26 @@ static int cpufreq_times_create_table(struct cpufreq_policy *policy)
 	times->last_index =
 		cpufreq_frequency_table_get_index(policy, policy->cur);
 	times->max_state = count;
+
+	spin_lock(&cpufreq_times_lock);
+	tmp = krealloc(cpufreq_states,
+		       sizeof(cpufreq_states[0]) * (next_offset + count),
+		       GFP_ATOMIC);
+	if (tmp) {
+		cpufreq_states = tmp;
+		cpufreq_for_each_entry(pos, table) {
+			cpufreq_states[next_offset + pos - table] =
+				pos->frequency;
+		}
+		times->offset = next_offset;
+		next_offset += count;
+	}
+	spin_unlock(&cpufreq_times_lock);
+	if (!tmp) {
+		kfree(times->time_in_state);
+		kfree(times);
+		return -ENOMEM;
+	}
 
 	policy->times = times;
 #ifdef CONFIG_CPU_FREQ_STAT
@@ -166,11 +246,16 @@ void cpufreq_times_record_transition(struct cpufreq_policy *policy,
 
 static int __init cpufreq_times_init(void)
 {
+	int ret;
 	unsigned int cpu;
 
-	spin_lock_init(&cpufreq_times_lock);
 	for_each_online_cpu(cpu)
 		cpufreq_times_create_policy(cpu);
+
+	ret = sysfs_create_file(cpufreq_global_kobject,
+			&_attr_all_time_in_state.attr);
+	if (ret)
+		pr_warn("Cannot create sysfs file for cpufreq stats\n");
 
 	return 0;
 }
